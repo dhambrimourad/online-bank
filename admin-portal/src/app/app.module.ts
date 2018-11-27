9297ab7b74014d70eb34fbb12e3b948098953c17
dhambrimourad <36987982+dhambrimourad@users.noreply.github.com>
2018-11-27 21:18:01 +0100

    Add files via upload

---
+++ admin-portal/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { UserAccountComponent } from './user-account/user-account.component';
+
+import { LoginService } from './login.service';
+import { UserService } from './user.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    UserAccountComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [
+    LoginService,
+    UserService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ admin-portal/src/app/app.routing.ts
@@ -0,0 +1,23 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { UserAccountComponent } from './user-account/user-account.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'user-account',
+    component: UserAccountComponent
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
